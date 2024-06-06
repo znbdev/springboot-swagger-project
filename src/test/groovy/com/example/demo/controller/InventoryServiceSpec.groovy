aaf4dec971abb8d8b46086ea505631a4a1da701e
znbdev <znbdev@outlook.com>
2024-06-06 22:50:31 +0900

    Modify

--- src/test/groovy/com/example/demo/controller/InventoryServiceSpec.groovy
+++ src/test/groovy/com/example/demo/controller/InventoryServiceSpec.groovy
@@ -1,64 +1,66 @@
-//package com.example.demo.controller
-//
-//import com.example.demo.entity.Inventory
-//import spock.lang.Specification
-//import spock.lang.Subject
-//import com.example.demo.repository.InventoryRepository
-//import com.example.demo.service.InventoryService
-//
-//// 1. Spock 框架的单元测试代码
-//class InventoryServiceSpec extends Specification {
-//
-//    @Subject
-//    InventoryService inventoryService
-//
-//    InventoryRepository inventoryRepository = Mock()
-//
-//    def setup() {
-//        inventoryService = new InventoryService(inventoryRepository)
-//    }
-//
-//    def "test purchaseProduct success"() {
-//        given: "a product ID and an inventory with sufficient quantity"
-//        Long productId = 1L
-//        Inventory inventory = new Inventory(productId: productId, quantity: 1)
-//
-//        when: "the service is called to purchase the product"
-//        inventoryRepository.findByIdForUpdate(productId) >> Optional.of(inventory)
-//        inventoryService.purchaseProduct(productId)
-//
-//        then: "the quantity is decreased and inventory is saved"
-//        1 * inventoryRepository.save(_) >> { Inventory savedInventory ->
-//            assert savedInventory.quantity == 0
-//        }
-//    }
-//
-//    def "test purchaseProduct throws exception when insufficient inventory"() {
-//        given: "a product ID and an inventory with zero quantity"
-//        Long productId = 1L
-//        Inventory inventory = new Inventory(productId: productId, quantity: 0)
-//
-//        when: "the service is called to purchase the product"
-//        inventoryRepository.findByIdForUpdate(productId) >> Optional.of(inventory)
-//        inventoryService.purchaseProduct(productId)
-//
-//        then: "a RuntimeException is thrown"
-//        thrown(RuntimeException)
-//        0 * inventoryRepository.save(_)
-//    }
-//
-//    def "test cancelProduct success"() {
-//        given: "a product ID and an inventory"
-//        Long productId = 1L
-//        Inventory inventory = new Inventory(productId: productId, quantity: 1)
-//
-//        when: "the service is called to cancel the product"
-//        inventoryRepository.findByIdForUpdate(productId) >> Optional.of(inventory)
-//        inventoryService.cancelProduct(productId)
-//
-//        then: "the quantity is increased and inventory is saved"
-//        1 * inventoryRepository.save(_) >> { Inventory savedInventory ->
-//            assert savedInventory.quantity == 2
-//        }
-//    }
-//}
+package com.example.demo.controller
+
+import com.example.demo.entity.Inventory
+import spock.lang.Specification
+import spock.lang.Subject
+import com.example.demo.repository.InventoryRepository
+import com.example.demo.service.InventoryService
+
+// 1. Spock 框架的单元测试代码
+class InventoryServiceSpec extends Specification {
+
+    @Subject
+    InventoryService inventoryService
+
+    InventoryRepository inventoryRepositoryMock = Mock()
+
+    def setup() {
+        inventoryService = new InventoryService(inventoryRepositoryMock)
+    }
+
+    def "test purchaseInventory success"() {
+        given: "a inventory ID and an inventory with sufficient quantity"
+        Long inventoryId = 1L
+        Inventory inventory = new Inventory(inventoryId: inventoryId, quantity: 1, version: 1)
+
+        when: "the service is called to purchase the inventory"
+        inventoryRepositoryMock.findByIdForUpdate(inventoryId) >> Optional.of(inventory)
+        inventoryRepositoryMock.findById(inventoryId) >> Optional.of(inventory)
+        inventoryService.purchaseInventory(inventoryId, 1)
+
+        then: "the quantity is decreased and inventory is saved"
+        1 * inventoryRepositoryMock.save(_) >> { Inventory savedInventory ->
+            assert savedInventory.quantity == 0
+        }
+    }
+
+    def "test purchaseInventory throws exception when insufficient inventory"() {
+        given: "a inventory ID and an inventory with zero quantity"
+        Long inventoryId = 1L
+        Inventory inventory = new Inventory(inventoryId: inventoryId, quantity: 0)
+
+        when: "the service is called to purchase the inventory"
+        inventoryRepositoryMock.findByIdForUpdate(inventoryId) >> Optional.of(inventory)
+        inventoryService.purchaseInventory(inventoryId)
+
+        then: "a RuntimeException is thrown"
+        thrown(RuntimeException)
+        0 * inventoryRepositoryMock.save(_)
+    }
+
+    def "test cancelInventory success"() {
+        given: "a inventory ID and an inventory"
+        Long inventoryId = 1L
+        Inventory inventory = new Inventory(inventoryId: inventoryId, quantity: 1)
+
+        when: "the service is called to cancel the inventory"
+        inventoryRepositoryMock.findByIdForUpdate(inventoryId) >> Optional.of(inventory)
+        inventoryRepositoryMock.findById(inventoryId) >> Optional.of(inventory)
+        inventoryService.cancelInventory(inventoryId, 1)
+
+        then: "the quantity is increased and inventory is saved"
+        1 * inventoryRepositoryMock.save(_) >> { Inventory savedInventory ->
+            assert savedInventory.quantity == 2
+        }
+    }
+}
