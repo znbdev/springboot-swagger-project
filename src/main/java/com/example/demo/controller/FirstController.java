ce20ee34dbe1abdac0d00ca51195805a26f24f5e
znbdev <znbdev@outlook.com>
2024-06-06 22:49:40 +0900

    Add i18n message

--- src/main/java/com/example/demo/controller/FirstController.java
+++ src/main/java/com/example/demo/controller/FirstController.java
@@ -4,9 +4,12 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -14,11 +17,16 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(tags = "First API")
 @RequestMapping("/api/first")
 public class FirstController {
+    private final MessageSource messageSource;
 
     @ApiOperation(value = "Get method in the first API")
     @GetMapping("/get")
     public String getFirstApi() {
         log.info("This is the first API");
+        log.info(messageSource.getMessage("api.name", null, Locale.getDefault()));
+        log.info(messageSource.getMessage("api.name", null, Locale.CHINA));
+        log.info(messageSource.getMessage("api.name", null, Locale.JAPAN));
+        log.info(messageSource.getMessage("api.name", null, Locale.US));
         return "This is the first API";
     }
 }
